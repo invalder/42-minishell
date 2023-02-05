1b650be30b9b01ce0bfef631c8ff30a003ec4bf4
Sirilert Thitikul <rubii@Rubii-Mac.local>
2023-02-06 01:30:49 +0700

    delete duplicate functions

--- srcs/ms_parser.c
+++ srcs/ms_parser.c
@@ -6,7 +6,7 @@
 /*   By: sthitiku <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 01:04:35 by sthitiku          #+#    #+#             */
-/*   Updated: 2023/02/06 01:13:21 by sthitiku         ###   ########.fr       */
+/*   Updated: 2023/02/06 01:25:46 by sthitiku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,63 +14,63 @@
 
 extern int	g_status;
 
-static char	*cut_quote(char *str, char quote)
-{
-	int		i;
-	int		len;
-	char	*new;
+// static char	*cut_quote(char *str, char quote)
+// {
+// 	int		i;
+// 	int		len;
+// 	char	*new;
 
-	i = 0;
-	len = 0;
-	new = NULL;
-	while (str[i])
-	{
-		if (str[i] != quote)
-			len++;
-		i++;
-	}
-	new = malloc(sizeof(char) * len + 1);
-	i = 0;
-	len = 0;
-	while (str[i])
-	{
-		if (str[i] != quote)
-			new[len++] = str[i];
-		i++;
-	}
-	new[len] = '\0';
-	free(str);
-	return (new);
-}
+// 	i = 0;
+// 	len = 0;
+// 	new = NULL;
+// 	while (str[i])
+// 	{
+// 		if (str[i] != quote)
+// 			len++;
+// 		i++;
+// 	}
+// 	new = malloc(sizeof(char) * len + 1);
+// 	i = 0;
+// 	len = 0;
+// 	while (str[i])
+// 	{
+// 		if (str[i] != quote)
+// 			new[len++] = str[i];
+// 		i++;
+// 	}
+// 	new[len] = '\0';
+// 	free(str);
+// 	return (new);
+// }
 
-static char	*parse_env_get_env(char *str, int str_len, char first, char last)
-{
-	char	*new;
-	t_parse	p;
+// static char	*parse_env_get_env(char *str, int str_len, char first, char last)
+// {
+// 	char	*new;
+// 	t_parse	p;
 
-	p.start = 0;
-	p.end = 0;
-	if (first == '\'' && last == '\'')
-	{
-		p.start = 2;
-		p.end = str_len - 2;
-		p.sub = ft_substr(str, p.start, p.end - p.start + 1);
-		p.env = getenv(p.sub);
-		new = malloc(sizeof(char) * (ft_strlen(p.env) + 3));
-		ft_strlcpy(new, "\'", 2);
-		ft_strlcpy(&new[1], p.env, ft_strlen(p.env) + 1);
-		ft_strlcpy(&new[ft_strlen(p.env) + 1], "\'", 2);
-	}
-	else
-	{
-		p.sub = ft_substr(str, p.start + 1, str_len - p.start - 1);
-		parse_env_exit_status(&p, str_len);
-		new = malloc(sizeof(char) * (ft_strlen(p.env) + 1));
-		ft_strlcpy(new, p.env, ft_strlen(p.env) + 1);
-	}
-	free(p.sub);
-	return (new);
-}
+// 	p.start = 0;
+// 	p.end = 0;
+// 	if (first == '\'' && last == '\'')
+// 	{
+// 		p.start = 2;
+// 		p.end = str_len - 2;
+// 		p.sub = ft_substr(str, p.start, p.end - p.start + 1);
+// 		p.env = getenv(p.sub);
+// 		new = malloc(sizeof(char) * (ft_strlen(p.env) + 3));
+// 		ft_strlcpy(new, "\'", 2);
+// 		ft_strlcpy(&new[1], p.env, ft_strlen(p.env) + 1);
+// 		ft_strlcpy(&new[ft_strlen(p.env) + 1], "\'", 2);
+// 	}
+// 	else
+// 	{
+// 		p.sub = ft_substr(str, p.start + 1, str_len - p.start - 1);
+// 		parse_env_exit_status(&p, str_len);
+// 		new = malloc(sizeof(char) * (ft_strlen(p.env) + 1));
+// 		ft_strlcpy(new, p.env, ft_strlen(p.env) + 1);
+// 	}
+// 	free(p.sub);
+// 	return (new);
+// }
 
 static char	*parse_env(char *str)
 {
